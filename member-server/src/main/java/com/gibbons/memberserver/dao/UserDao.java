4e744e04d17266703abbf4442cc54c1880be8340
xjz <xjz@qq.com>
2018-02-09 10:05:10 +0800

    datasource druid

---
+++ member-server/src/main/java/com/gibbons/memberserver/dao/UserDao.java
@@ -0,0 +1,27 @@
+/**
+ * FileName: UserDao
+ * Author:   xiangjunzhong
+ * Date:     2018/2/9 9:39
+ * Description: 用户Dao
+ */
+package com.gibbons.memberserver.dao;
+
+import com.gibbons.memberserver.entity.User;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈用户Dao〉
+ *
+ * @author xiangjunzhong
+ * @create 2018/2/9 9:39
+ * @since 1.0.0
+ */
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+}
